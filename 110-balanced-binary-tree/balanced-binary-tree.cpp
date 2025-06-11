e012aa2442669a63a1b88c08de26c31de76e1215
rohit2026 <142927946+rohit2026@users.noreply.github.com>
2025-06-11 16:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (84.13%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,31 +1,72 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+
+
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+//     int BTheight(TreeNode* root){
+//         if(root==nullptr) return 0;
+//         int lh=BTheight(root->left);
+//         int rh=BTheight(root->right);
+//         return 1+max(lh,rh);
+//     }
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         if(root==nullptr) return true;
+
+//         int lh=BTheight(root->left);
+//         int rh=BTheight(root->right);
+//         if(abs(lh-rh)>1) return false;
+
+//         bool b1=isBalanced(root->left);
+//         bool b2=isBalanced(root->right);
+//         return b1&&b2;
+//     }
+// };
+
+
+
+
+
 class Solution {
-    int BTheight(TreeNode* root){
+public:
+    int dfH(TreeNode* root) {
         if(root==nullptr) return 0;
-        int lh=BTheight(root->left);
-        int rh=BTheight(root->right);
+
+        int lh=dfH(root->left);
+        int rh=dfH(root->right);
+        if(lh==-1 || rh==-1) return -1;
+
+        if(abs(lh-rh)>1) return -1;
         return 1+max(lh,rh);
     }
-public:
+
+
+
     bool isBalanced(TreeNode* root) {
-        if(root==nullptr) return true;
-
-        int lh=BTheight(root->left);
-        int rh=BTheight(root->right);
-        if(abs(lh-rh)>1) return false;
-
-        bool b1=isBalanced(root->left);
-        bool b2=isBalanced(root->right);
-        return b1&&b2;
+        int check=dfH(root);
+        if(check!=-1) return true;
+        else return false;
     }
 };
