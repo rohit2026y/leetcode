65ffcc1dbc1c0978cd73b5a32e9ed5db1759a00b
rohit2026y <142927946+rohit2026y@users.noreply.github.com>
2025-08-12 20:04:50 +0530

    Time: 601 ms (27.94%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,8 +1,9 @@
+
 select distinct num as ConsecutiveNums
-from (select *,
-lag(num,1) over(order by id) as lag1,
-lag(num,2) over(order by id) as lag2,
+from (select id,num,
 lead(num,1) over(order by id) as lead1,
-lead(num,2) over(order by id) as lead2
-from logs) as t
-where (num=lag1 and num=lag2) or (num=lead1 and num=lead2) or (num=lead1 and num=lag1);
+lead(num,2) over(order by id) as lead2,
+lag(num,1) over(order by id) as lag1,
+lag(num,2) over(order by id) as lag2
+from logs) as alias
+where (num=lag1 and num= lag2) or (num=lead1 and num=lead2) or (num=lead1 and num=lag1);
